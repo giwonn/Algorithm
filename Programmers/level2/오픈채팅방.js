5972c54d21517d85853e09ee0f19ad26d914c5e2
임기원 <giwon.dev@gmail.com>
2021-12-05 22:21:12 +0900

    Update 오픈채팅방.js

--- Programmers/level2/오픈채팅방.js
+++ Programmers/level2/오픈채팅방.js
@@ -1,24 +1,16 @@
-// https://programmers.co.kr/learn/courses/30/lessons/42888
 function solution(record) {
-    // {유저아이디: 닉네임, ...} 형식으로 저장함
     const id_nickname = {};
+    
     record.forEach((log) => {
-        // 행동, 유저아이디, 닉네임
         const [behavier, userid, nickname] = log.split(' ');
-        if (['Change', 'Enter'].includes(behavier)) {
-            id_nickname[userid] = nickname;
-        }
+        if (['Change', 'Enter'].includes(behavier)) id_nickname[userid] = nickname;
     })
     
     return record
-        .filter((log) => log.split(' ')[0] !== 'Change')    // filter로 change는 제외
-        .map((log) => {                                     // map으로 새로운 배열 리턴
-        const [behavier, userid, nickname] = log.split(' ');
-        if (behavier === 'Enter') {
-            return `${id_nickname[userid]}님이 들어왔습니다.`;
-        }
-        if (behavier === 'Leave') {
-            return `${id_nickname[userid]}님이 나갔습니다.`;
-        }
-    })
+        .filter((log) => log.split(' ')[0] !== 'Change')
+        .map((log) => {
+            const [behavier, userid, nickname] = log.split(' ');
+            if (behavier === 'Enter') return `${id_nickname[userid]}님이 들어왔습니다.`;
+            if (behavier === 'Leave') return `${id_nickname[userid]}님이 나갔습니다.`;
+        });
 }
