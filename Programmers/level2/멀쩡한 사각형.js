cddc39a617307cc840e636529890b2f0dc556110
임기원 <giwon.dev@gmail.com>
2021-12-07 22:48:23 +0900

    Create 멀쩡한 사각형.js

---
+++ Programmers/level2/멀쩡한 사각형.js
@@ -0,0 +1,10 @@
+// 대각선이 지나가는 사각형의 넓이 = 가로 + 세로 - (최대공약수)
+// 답 = 전체사각형 넓이 - 대각선지나는넓이
+// 최대공약수 : 유클리드 호제법을 사용하자..
+function solution(w, h) {
+    return w * h - (w + h - gcd(w, h));
+}
+
+function gcd(w, h) {
+    return w % h === 0 ? h : gcd(h, w % h);
+}
