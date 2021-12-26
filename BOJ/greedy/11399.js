5b102ed30f1cf52655336397d14d31904ae3655d
임기원 / Allen <giwon.dev@gmail.com>
2021-12-26 22:32:37 +0900

    Update 11399.js

--- BOJ/greedy/11399.js
+++ BOJ/greedy/11399.js
@@ -1,15 +1,11 @@
 var fs = require('fs');
 var [n, input] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let res = 0;
-let answer = input
-  .split(' ')
-  .sort((a, b) => a - b)
+let answer = 0;
+answer = input.split(' ').sort((a, b) => a - b)
   .reduce((acc, cur, idx) => {
-    res += acc;
-    return acc + Number(cur);
-  }, 0);
-
-answer += res;
+      answer += acc;
+      return acc + Number(cur);
+    }, 0) + answer;
 
 console.log(answer);
